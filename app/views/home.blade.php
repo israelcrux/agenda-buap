85923c0bae19dfc6d25fd021208cd954ab096118
dantaex <jesuslink00@gmail.com>
2014-11-06 12:56:13 -0600

    login bonito

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -10,4 +10,9 @@
 	<h1 class="ar-hidden">Dirección de Comunicación Institucional</h1>
 	<h3 class="ar-hidden">Calendario de Eventos</h3>
 	@include('calendar')
+	<section>
+		<h3>Solicitud de Difusión</h3>
+		<p></p>
+		
+	</section>
 @stop
