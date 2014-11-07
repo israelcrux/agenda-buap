a0979c33ea515ac279c69db73f05394483cf16d1
dantaex <jesuslink00@gmail.com>
2014-11-07 13:48:02 -0600

    minor changes

--- app/views/signup.blade.php
+++ app/views/signup.blade.php
@@ -5,7 +5,7 @@
 		<div class="ar-form-container">
 			<h1>Registro</h1>
 		    {{ Form::open(array('url' => '/signup', 'method' => 'post', 'role'=>'form', 'class' =>'form-horizontal' )); }}
-				<input class="form-control" name="name" type="text" id="name" placeholder="Nombre" autocomplete="off">
+				<input class="form-control" name="name" type="text" id="name" placeholder="Nombre" autocomplete="off" required="true">
 				<input class="form-control" name="phone" type="text" id="phone" placeholder="Teléfono" autocomplete="off">
 				<select name="unidad_academica" id="">
 					<option value="">Unidad Acádemica/Administrativa</option>
@@ -13,7 +13,6 @@
 						<option value="">{{ $aaunit['name'] }}</option>
 					@endforeach
 				</select>
-
 				<input class="form-control" name="username" type="text" id="username" placeholder="E-mail" autocomplete="off" required="true">
 				<input class="form-control" name="password" type="password" value="" id="password" placeholder="Contraseña" required="true">
 				<input class="form-control" type="submit" value="Registrarse">
