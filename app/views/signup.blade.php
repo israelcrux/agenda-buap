a9ae601cb52036f39b7060a9597bd79714792930
Javier Aguila <sjaca10@gmail.com>
2014-11-07 14:08:38 -0600

    Login, registro

--- app/views/signup.blade.php
+++ app/views/signup.blade.php
@@ -7,13 +7,12 @@
 		    {{ Form::open(array('url' => '/signup', 'method' => 'post', 'role'=>'form', 'class' =>'form-horizontal' )); }}
 				<input class="form-control" name="name" type="text" id="name" placeholder="Nombre" autocomplete="off">
 				<input class="form-control" name="phone" type="text" id="phone" placeholder="Teléfono" autocomplete="off">
-				<select name="unidad_academica" id="" placeholder="">
+				<select name="unidad_academica" id="">
 					<option value="">Unidad Acádemica/Administrativa</option>
 					@foreach($aaunits as $aaunit)
 						<option value="">{{ $aaunit['name'] }}</option>
 					@endforeach
 				</select>
-
 				<input class="form-control" name="username" type="text" id="username" placeholder="E-mail" autocomplete="off" required="true">
 				<input class="form-control" name="password" type="password" value="" id="password" placeholder="Contraseña" required="true">
 				<input class="form-control" type="submit" value="Registrarse">
