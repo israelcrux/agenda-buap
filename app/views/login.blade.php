23c857e1ddb4d7fbbf0d141b35c9e85f26798206
dantaex <jesuslink00@gmail.com>
2014-11-07 11:22:43 -0600

    wea fome

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -1,13 +1,27 @@
 @extends('layout')
 
 @section('content')
-    {{ Session::get('alert'); }}
+	<?php 
+		$alert = Session::get('alert');
+		if($alert){ 
+	?>
+   	<div id="ar-alrt" class="ar-alert in"><?php echo $alert ?></div>
+   	<script>
+   		setTimeout(function(){
+   			document.getElementById('ar-alrt').className="ar-alert out";
+   		},3000);
+   	</script>
+	<?php 
+		} 
+	?>
+    <div class="ar-alert">Testing</div>
+
 	<div class="ar-form-wrapper ar-login-form">
 		<div class="ar-form-container">
 			<h1>Entrar</h1>
 		    {{ Form::open(array('url' => '/login', 'method' => 'post', 'role'=>'form', 'class' =>'form-horizontal' )); }}
-				<input class="form-control" name="username" type="text" id="username" placeholder="e-mail" autocomplete="off">
-				<input class="form-control" name="password" type="password" value="" id="password" placeholder="password">
+				<input class="form-control" name="username" type="text" id="username" placeholder="e-mail" autocomplete="off" required="true">
+				<input class="form-control" name="password" type="password" value="" id="password" placeholder="password" required="true">
 				<input class="form-control" type="submit" value="Entrar">
 		    {{ Form::close(); }}		    
 		</div>
--- app/views/signup.blade.php
+++ app/views/signup.blade.php
@@ -1,23 +1,22 @@
 @extends('layout')
 
 @section('content')
-    {{ Session::get('alert'); }}
-	<div class="ar-form-wrapper">
+    <div class="ar-form-wrapper">
 		<div class="ar-form-container">
 			<h1>Registro</h1>
 		    {{ Form::open(array('url' => '/signup', 'method' => 'post', 'role'=>'form', 'class' =>'form-horizontal' )); }}
 
-				<input class="form-control" name="name" type="text" id="name" placeholder="Nombre" autocomplete="off">
-				<input class="form-control" name="puesto" type="text" id="puesto" placeholder="Área" autocomplete="off">
-				<input class="form-control" name="area" type="text" id="area" placeholder="Puesto" autocomplete="off">
-				<input class="form-control" name="area" type="text" id="phone" placeholder="Teléfono" autocomplete="off">
+				<input class="form-control" name="name" type="text" id="name" placeholder="Nombre" autocomplete="off" required="true">
+				<input class="form-control" name="puesto" type="text" id="puesto" placeholder="Área" autocomplete="off" >
+				<input class="form-control" name="area" type="text" id="area" placeholder="Puesto" autocomplete="off" >
+				<input class="form-control" name="phone" type="text" id="phone" placeholder="Teléfono" autocomplete="off">
 
 				<select name="unidad_academica" id="">
 					<option value="">ola</option>
 					<option value="">k ase</option>
 				</select>
-				<input class="form-control" name="username" type="text" id="username" placeholder="E-mail" autocomplete="off">
-				<input class="form-control" name="password" type="password" value="" id="password" placeholder="password">
+				<input class="form-control" name="username" type="text" id="username" placeholder="E-mail" autocomplete="off" required="true">
+				<input class="form-control" name="password" type="password" value="" id="password" placeholder="password" required="true">
 				<input class="form-control" type="submit" value="Registrarse">
 		    {{ Form::close(); }}		    
 		</div>
