c0e0efd677182feb1f108f7f4ac669e6c1d1120b
dantaex <jesuslink00@gmail.com>
2014-11-07 17:10:53 -0600

    SWAGG

--- app/views/login.blade.php
+++ app/views/login.blade.php
@@ -24,19 +24,19 @@
 				<input class="form-control" name="password" type="password" value="" id="password" placeholder="Contraseña" required="true">
 				<input class="form-control" type="submit" value="Entrar">
 		    {{ Form::close(); }}		    
-			<section>
-				<h3>Para agendar eventos y solicitar difusión</h3>
-				<p>
-					La Dirección de Comunicación Institucional de la BUAP ofrece diversos servicios destinados
-					a la difusión de eventos y noticias relacionadas con la vida universitaria.
-					Si usted organiza o participa en la organización de algún evento, puede solicitar la difusión del
-					mismo a través de éste portal; Para ello es necesario contar con un correo institucional 
-					(con dominio terminado en .buap.mx) y crear una cuenta
-				</p>
-				<p>
-					<button class="btn btn-primary" onclick="document.location='{{ URL::to('/registro') }}'">Solicitar Difusión</button>
-				</p>
-			</section>
 		</div>
 	</div>
+	<section class="ar-reg-msg">
+		<h3>Para agendar eventos y solicitar difusión</h3>
+		<p>
+			La Dirección de Comunicación Institucional de la BUAP ofrece diversos servicios destinados
+			a la difusión de eventos y noticias relacionadas con la vida universitaria.
+			Si usted organiza o participa en la organización de algún evento, puede solicitar la difusión del
+			mismo a través de éste portal; Para ello es necesario contar con un correo institucional 
+			(con dominio terminado en .buap.mx) y crear una cuenta
+		</p>
+		<p>
+			<button class="btn btn-primary" onclick="document.location='{{ URL::to('/signup') }}'">Registrarse</button>
+		</p>
+	</section>
 @stop
