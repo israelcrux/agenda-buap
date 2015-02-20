f21d18f3c78d2609c1b726ca2e2c62f2b09600db
dantaex <jesuslink00@gmail.com>
2015-02-19 18:36:27 -0600

    weas olvidadas de pushear

--- app/views/dashboard-admin-angular.php
+++ app/views/dashboard-admin-angular.php
@@ -60,6 +60,52 @@
 			<div class="ar-modal-closebtn" ng-click="showUserForm=false"></div>
 			<div class="ar-modal-content">
 				
+				<?php echo Form::open(array('url' => '/register', 'method' => 'post', 'role'=>'form', 'class' =>'form-horizontal' )); ?>
+					<input class="form-control" name="first_name" type="text" id="first_name" placeholder="Nombre(s)" autocomplete="off" required>
+					<input class="form-control" name="last_name" type="text" id="last_name" placeholder="Apellidos" autocomplete="off" required>
+					<input class="form-control" name="phone" type="text" id="phone" placeholder="Teléfono" autocomplete="off" pattern="([0-9]+|-|\s)+" title="Ejemplos: 888888, 8-888-888, 8 888 888">
+					<input class="form-control" name="extension_phone" type="text" id="extension_phone" placeholder="Teléfono/Extensión Buap" autocomplete="off" >
+					<select name="academic_administrative_unit_type" id="aaut" value="" required>
+						<option value="0">
+							Seleccionar Procedencia
+						</option>
+						<option value="1">
+							Unidad Administrativa
+						</option>
+						<option value="2">
+							Unidad Académica
+						</option>
+						<option value="3">
+							Otro
+						</option>
+					</select>
+
+					<select name="academic_administrative_unit" id="acadun" value="">
+						<option value="">Ninguna</option>
+						<?php foreach($aaunits as $aaunit): ?>
+							<option value="<?php echo $aaunit['id'] ?>"><?php echo $aaunit['name'] ?></option>
+						<?php endforeach; ?>
+					</select>
+
+					<select name="user_type_id" id="utunits" value="" required>
+						<option value="0">Seleccionar Nivel de Usuario</option>
+						<?php foreach($utunits as $utunit): ?>
+							<option value="<?php echo $utunit['id'] ?>"><?php echo $utunit['name'] ?></option>
+						<?php endforeach; ?>
+					</select>
+
+					<select name="department_id" id="dunits" value="" required>
+						<option value="0">Seleccionar Departamento</option>
+						<?php foreach($dunits as $dunit): ?>
+							<option value="<?php echo $dunit['id'] ?>"><?php echo $dunit['name'] ?></option>
+						<?php endforeach; ?>
+					</select>
+					
+					<input class="form-control" name="email" type="email" id="username" placeholder="E-mail" autocomplete="off" required>
+					<input class="form-control" name="password" type="password" value="" id="password" placeholder="Contraseña" required>
+					<input class="form-control" type="submit" value="Registrarse">
+				<?php echo Form::close(); ?>
+
 			</div>
 		</div>
 	</div>
@@ -110,6 +156,7 @@ var users_app = angular.module('dashboard',[])
 	}])
 	.controller('UsersController',['$scope','DataService',function($scope,DataService){
 
+		$scope.showUserForm = false;
 		$scope.userTypes = {
 			'1' :  'Cliente',
 			'2' :  'Empleado DCI',
