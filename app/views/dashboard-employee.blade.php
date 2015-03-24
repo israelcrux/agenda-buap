ff7433e6ae982cacccff44e2deb09a8bb27a6a72
Israel <israel@aerostato.mx>
2015-03-23 18:18:34 -0600

    error ortográfico en dash-employee

--- app/views/dashboard-employee.blade.php
+++ app/views/dashboard-employee.blade.php
@@ -5,7 +5,7 @@
     <div class="ar-vwrap">
     	<div class="col-xs-12 col-sm-10">
     		<h3>Hola {{ Auth::user()->first_name }}</h3>
-    		<p>Estas son la stareas que tienes pendientes</p>
+    		<p>Estas son las tareas que tienes pendientes</p>
     	</div>
     </div>
 
