7eecce759651facb6d8a8bde4ee78bd6756c28d1
Israel <israel@aerostato.mx>
2015-04-22 13:33:21 -0500

    Error fatal de CSS que ocultaba archivos adjuntos

--- app/views/event-view.blade.php
+++ app/views/event-view.blade.php
@@ -24,7 +24,7 @@
 
 		<div class="col-sm-4">
 			<div class="ar-over-title">Hora</div>
-			<p>{{$event->time}}</p>
+			<p>{{$event->time}} hrs.</p>
 		</div>
 
 
@@ -42,35 +42,52 @@
 	<div class="ar-row">
 		<div class="col-sm-4">
 			<div class="ar-over-title">Evento gratuito?</div>
-			<p>{{$event->has_cost}}</p>
+			<?php if($event->has_cost): ?>
+				<b>Sí, Evento gratuito</b>
+			<?php else: ?>
+				<b>No, El evento tiene costo</b>
+			<?php endif; ?>
 		</div>
 
 		<div class="col-sm-4">
 			<div class="ar-over-title">Dirigido a</div>
 			<p>{{$event->directed_to}}</p>
 		</div>
-	</div>
 	
 
-	<div class="col-xs-12">
-		
+		<div class="col-xs-12">
+			
+			<?php if($event->link): ?>
+				<div class="ar-over-title">Sitio web</div>
+				<p>{{$event->link}}</p>
+			<?php endif; ?>
 
-		<div class="ar-over-title">Sitio web</div>
-		<p>{{$event->link}}</p>
+			<div class="ar-over-title">Descripción</div>
+			<p>{{$event->description}}</p>
 
-		<div class="ar-over-title">Descripción</div>
-		<p>{{$event->description}}</p>
-
+		</div>
 	</div>
 
-
+	
 	<?php if(isset($event->support_materials)): ?>
-		<?php foreach ($event->support_materials as $file): ?>
-			<div class="ar-row ar-boldrow">
-				<div class="col-xs-12 col-sm-8"><a target="_blank" href="{{URL::to('/')}}/{{$file->file}}">{{$file->original_name}}</a></div>
-				<div class="col-xs-12 col-sm-4">{{$file->created_at}}</div>
-			</div>
-		<?php endforeach; ?>
+	<div>
+		<p>Archivos aduntos: {{count($event->support_materials)}}</p>
+			<?php foreach ($event->support_materials as $file): ?>
+				<div class="ar-row ar-boldrow">
+					<div class="col-xs-2 col-sm-2">
+						<div class="ar-format-bullet">
+							<?php
+								if( preg_match('/\..{2,5}$/', $file->file, $ext) ){
+									echo substr($ext[0], 1, strlen($ext[0]) -1 );
+								}
+							?>
+						</div>
+					</div>
+					<div class="col-xs-10 col-sm-6"><a target="_blank" href="{{URL::to('/')}}/{{$file->file}}">{{$file->original_name}}</a></div>
+					<div class="col-xs-10 col-sm-4">{{$file->created_at}}</div>
+				</div>
+			<?php endforeach; ?>
+	</div>
 	<?php endif; ?>
 
 
